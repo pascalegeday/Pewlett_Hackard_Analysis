86a4783cb2e50bcaae4564c45a1a25591afb76da
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-01-08 23:35:55 -0800

    Upload Data From First Query

--- schema.sql
+++ schema.sql
@@ -42,7 +42,7 @@ CREATE TABLE salaries (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no, to_date)
  );
 
 CREATE TABlE titles (
@@ -51,11 +51,13 @@ CREATE TABlE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, to_date)
 );
 
 SELECT
 	*
 FROM
 	departments;
-	
+
+-- DROP TABLE dept_emp CASCADE;
+-- DROP TABLE titles CASCADE;
