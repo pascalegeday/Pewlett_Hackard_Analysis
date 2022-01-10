5f97472864079ccfdd4b6b000671eae6a1a222b5
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-01-10 13:50:55 -0800

    Upload 2 Queries for Further Data Insight

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -43,8 +43,8 @@ SELECT
 COUNT
 	(ut.title),
 	ut.title
-INTO
-	retiring_titles
+-- INTO
+--	retiring_titles
 FROM
 	unique_titles as ut
 GROUP BY
@@ -63,8 +63,8 @@ DISTINCT ON
 	de.from_date,
 	de.to_date,
 	tt.title
-INTO
-	mentorship_eligibility	
+--INTO
+--	mentorship_eligibility
 FROM
 	employees as e
 INNER JOIN
@@ -80,3 +80,72 @@ WHERE
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY
 	e.emp_no;
+	
+	
+-- Total number of employees per title
+SELECT
+COUNT
+	(me.title),
+	me.title
+	
+--INTO
+--	mentors_per_title
+FROM
+	mentorship_eligibility as me
+GROUP BY
+	me.title
+ORDER BY
+	COUNT(me.title) DESC;
+
+
+
+
+
+
+-- Department each mentor falls under
+SELECT
+DISTINCT ON
+	(e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title,
+	d.dept_name
+-- INTO
+-- 	mentorship_eligibility_dept
+FROM
+	employees as e
+INNER JOIN
+ 	dept_emp as de
+ON
+	(e.emp_no = de.emp_no)
+INNER JOIN 
+	titles as tt
+ON 
+	(e.emp_no = tt.emp_no)
+INNER JOIN 
+	departments as d
+ON 
+	de.dept_no = d.dept_no
+WHERE
+	(de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY
+	e.emp_no;
+
+SELECT
+	me.title,
+	me.dept_name,
+COUNT
+	(me.title)
+--INTO
+-- mentors_per_dept
+FROM
+	mentorship_eligibility_dept as me
+GROUP BY
+	me.title
+ORDER BY
+	COUNT(me.title) DESC;
