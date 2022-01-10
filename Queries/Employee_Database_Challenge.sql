258502d11ba3565ccaf2eacfac9cab1ced07c8bd
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-01-09 18:30:48 -0800

    Upload Final Analysis for PH

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,82 @@
+-- Retrieve the titles of employees who were born bt. Jan 1952 and Dec 1955
+SELECT
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	tt.title,
+	tt.from_date,
+	tt.to_date
+--INTO
+--	retirement_titles
+FROM
+	employees as e 
+INNER JOIN
+	titles as tt 
+ON
+	e.emp_no = tt.emp_no
+WHERE
+	(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY
+	e.emp_no
+
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT 
+DISTINCT ON 
+	(rt.emp_no)
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+--INTO 
+--	unique_titles
+FROM
+	retirement_titles as rt
+WHERE
+	(rt.to_date = '9999-01-01')
+ORDER BY
+	rt.emp_no, rt.to_date DESC;
+
+-- Retrieve number of employees by their most recent job title who are about to retire
+SELECT
+COUNT
+	(ut.title),
+	ut.title
+INTO
+	retiring_titles
+FROM
+	unique_titles as ut
+GROUP BY
+	ut.title
+ORDER BY
+	COUNT(ut.title) DESC;
+	
+-- Mentorship-eligibility table holding current employees born bet. Jan 1965 and Dec 1965
+SELECT
+DISTINCT ON
+	(e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO
+	mentorship_eligibility	
+FROM
+	employees as e
+INNER JOIN
+	dept_emp as de
+ON 
+	(e.emp_no = de.emp_no)
+INNER JOIN 
+	titles as tt
+ON 
+	(e.emp_no = tt.emp_no)
+WHERE
+	(de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY
+	e.emp_no;
